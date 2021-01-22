88ab154d5ce1cdb77cacb39bf951774f56cfb218
mhssn <mhssn95@gmail.com>
2021-01-22 21:39:46 +0300

    update models

--- models.py
+++ models.py
@@ -3,7 +3,6 @@ from sqlalchemy import Column, String, Integer, Date, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-
 DATABASE_URI = 'postgres://aryggjifcqwmur:7cadc855538c0840ca9e79202056e899d3f1f0861543127a57dfd81807a1f64b@ec2-52-44-46-66.compute-1.amazonaws.com:5432/dep76g3od832hk'
 
 db = SQLAlchemy()
@@ -15,7 +14,6 @@ def setup_db(app, database_path=DATABASE_URI):
     db.app = app
     db.init_app(app)
     db.create_all()
-
 
 
 # Movies with attributes title and release date
@@ -42,13 +40,13 @@ class Movie(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-
     def format(self):
         return {
             'id': self.id,
             'title': self.title,
             'release_date': self.release_date,
         }
+
 
 # Actors with attributes name, age and gender
 class Actor(db.Model):
@@ -85,8 +83,9 @@ class Actor(db.Model):
             'gender': self.gender,
         }
 
-    actors_movies = db.Table(
-        'ActorsMovies',
-        db.Column('actor_id', db.Integer, db.ForeignKey('Actor.id'), primary_key=True),
-        db.Column('movie_id', db.Integer, db.ForeignKey('Movie.id'), primary_key=True),
-    )
+
+actors_movies = db.Table(
+    'ActorsMovies',
+    db.Column('actor_id', db.Integer, db.ForeignKey('Actor.id'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('Movie.id'), primary_key=True),
+)
